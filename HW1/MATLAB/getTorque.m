d30ed5d57ea0d06ff8d993840d7adb49266af068
yanshil <yanshi.luo@rutgers.edu>
2018-09-26 14:51:29 -0400

    modify

--- HW1/MATLAB/getTorque.m
+++
@@ -1,8 +0,0 @@
-function [torque] = getTorque(r,x,F)
-torque = 0;
-for i =1:8
-    torque = torque + cross((r(:,i) - x), F/8);
-end
-
-end
-
--- HW1/MATLAB/rotateRB.m
+++ HW1/MATLAB/rotateRB.m
@@ -82,6 +82,14 @@ while x >= -1
  end
 
 
+%% getTorque
 
+function [torque] = getTorque(r,x,F)
+torque = 0;
+for i =1:8
+    torque = torque + cross((r(:,i) - x), F/8);
+end
+
+end
 
 
