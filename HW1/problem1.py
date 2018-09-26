65f5e8cf89612b06155f01cfa640b0aabc96a7ae
Yanshi Luo <kdrobian@hotmail.com>
2018-09-26 14:46:16 -0400

    Report & P1

---
+++ HW1/problem1.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+## CS523.HW1.problem1
+## Yanshi Luo
+
+## Backward Euler
+def backwardEuler(u,v,h):
+    for i in range(1,100):
+    # Reference: https://stackoverflow.com/questions/22742951/solve-an-equation-using-a-python-numerical-solver-in-numpy
+        func = lambda tau : tau - u[i-1] - h * u[i-1] * ( (v[i-1] / (1- h * (1-tau))) - 2)
+
+        tau = np.linspace(-10, 10, 201)
+        tau_initial_guess = 0.5
+
+        u_new = fsolve(func, tau_initial_guess)
+
+        v_new = v[i-1] / (1 - h * (1 - u_new))
+
+        u.append(u_new)
+        v.append(v_new)
+
+    plt.plot(u,v,'ro')
+    plt.axis([0,6,0,10])
+    plt.title('u0=%s, v0=%s, h=%s' % (u[0], v[0], h))
+    plt.show()
+    return;
+
+backwardEuler([4],[8],.12)
+
+
+## Sympletic Euler
+def sympleticEuler(u,v,h):
+    for i in range(1,100):
+        v_new = v[i-1] + h * v[i-1] * (1.0 -u[i-1])
+        u_new = u[i-1] + h * u[i-1] * (v_new - 2)
+
+        u.append(u_new)
+        v.append(v_new)
+
+    plt.plot(u,v,'ro')
+    plt.axis([0,6,0,10])
+    plt.title('u0=%s, v0=%s, h=%s' % (u[0], v[0], h))
+    plt.show()
+    return;
+
+sympleticEuler([4],[2],.12)
+sympleticEuler([6],[2],.12)
+
